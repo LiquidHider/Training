abf89655e700eabc62b24f34af9e297919850abc
Vladyslav Maksymenko <maksymenko.vladyslav.o@hneu.net>
2023-02-18 16:03:05 +0200

    Add ClientController Update _Layout and addd index view for CleintController

---
+++ Training.Web/Controllers/ClientController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Training.Web.Data;
+using Training.Web.Models;
+
+namespace Training.Web.Controllers
+{
+    public class ClientController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        public ClientController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Client> objClientList = _db.Clients;
+            return View(objClientList);
+        }
+    }
+}
