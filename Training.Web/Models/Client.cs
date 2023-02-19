95897d2190d098c99f945eaeaad3082c3bb75c55
Selemetow Dmytro <syelyemyetov.dmytro.d@hneu.net>
2023-02-19 17:53:01 +0300

    Add regex to Client model.

--- Training.Web/Models/Client.cs
+++ Training.Web/Models/Client.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Training.Web.Models
@@ -9,13 +8,17 @@ namespace Training.Web.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Required , Display(Name = "П.І.Б"), MaxLength(50)]
         public string FullName  { get; set; }
+
         [Display(Name = "Серія Паспорту"), MaxLength(8)]
         public string PassportSerial { get; set; }
+
         [Display(Name = "Номер Паспорту"), MaxLength(9)]
         public string PassportNumber { get; set; }
-        [Display(Name = "Номер Телефону"), MaxLength(13)]
+
+        [Display(Name = "Номер Телефону"), MaxLength(13), RegularExpression(@"^\+380\d{9}$")]
         public string PhoneNumber { get; set; }
     }
 }
