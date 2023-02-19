106c7d5aebdb36dc3af1bbc5d50757486430695d
Vladyslav Maksymenko <maksymenko.vladyslav.o@hneu.net>
2023-02-19 12:40:54 +0200

    Add CRUD foe Clients tables

--- Training.Web/Controllers/ClientController.cs
+++ Training.Web/Controllers/ClientController.cs
@@ -16,5 +16,67 @@ namespace Training.Web.Controllers
             IEnumerable<Client> objClientList = _db.Clients;
             return View(objClientList);
         }
+        //GET
+        public IActionResult Upsert(int? id)
+        {
+            Client client = new();
+            if(id ==null || id == 0)
+            {
+                return View(client);
+            }
+            else
+            {
+                client = _db.Clients.FirstOrDefault(u => u.Id == id);
+                return View(client);
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Client obj)
+        {
+            if(ModelState.IsValid)
+            {
+                if(obj.Id == 0)
+                {
+                    _db.Clients.Add(obj);
+                }
+                else
+                {
+                    _db.Clients.Update(obj);
+                }
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(obj);
+        }
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var client = _db.Clients.FirstOrDefault(u => u.Id == id);
+            if(client == null)
+            {
+                return NotFound();
+            }
+            return View(client);
+        }
+
+        //POST
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+           var client = _db.Clients.FirstOrDefault(u => u.Id == id);
+           if(client == null)
+           {
+               return NotFound();
+           }
+            _db.Clients.Remove(client);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
