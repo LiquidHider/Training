cea70c3e95bc679f70ff1bca97289206ad6692c0
Selemetow Dmytro <syelyemyetov.dmytro.d@hneu.net>
2023-02-22 02:15:27 +0300

    Fix FileController. Add "Завантажити звіт" button.

--- Training.Web/Controllers/FileController.cs
+++ Training.Web/Controllers/FileController.cs
@@ -12,34 +12,49 @@ namespace Training.Web.Controllers
     public class FileController : Controller
     {
         private readonly ApplicationDBContext _db;
+        private readonly IWebHostEnvironment _env;
         private readonly IConfiguration _configuration;
-        public FileController(ApplicationDBContext db, IConfiguration configuration)
+        public FileController(ApplicationDBContext db, IConfiguration configuration, IWebHostEnvironment env)
         {
             _db = db;
             _configuration = configuration;
+            _env = env;
         }
 
-        [HttpPost, ActionName("Download")]
+        [HttpGet, ActionName("Download")]
         public ActionResult Download()
         {
             Generate();
-            string filePath = "/Content/Report.pdf";
             string fileName = "Report.pdf";
+            var contentFolder = Path.Combine(_env.ContentRootPath, "Content");
+            var filePath = Path.Combine(contentFolder, fileName);
+            
 
             byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
             return  File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
 
-            //< a href = "@Url.Action("DownloadFile", "ControllerName")" > Download File </ a >
+          
         }
 
         public void Generate()
         {
-            iTextSharp.text.Document Report = new iTextSharp.text.Document();
-            PdfWriter.GetInstance(Report, new FileStream("Report.pdf", FileMode.Create));
+            Document Report = new Document();
+            var contentFolder = Path.Combine(_env.ContentRootPath, "Content");
+            var filePath = Path.Combine(contentFolder, "Report.pdf");
+            if (!Directory.Exists(contentFolder))
+            {
+                Directory.CreateDirectory(contentFolder);
+            }
+            if (System.IO.File.Exists(filePath)) 
+            {
+                System.IO.File.Delete(filePath);
+            }
+
+            PdfWriter.GetInstance(Report, new FileStream(filePath, FileMode.Create));
             Report.Open();
             IConfigurationSection section = _configuration.GetSection("ConnectionStrings");
             string conn_str = section["DefaultConnection"];
-            SqlDataAdapter dataAdapter = new SqlDataAdapter("!!!!!!!!!!!!!!!!!", conn_str);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter("SELECT [Id], [Name],[Description], [AppraisedValue], [CategoryId], [Status] FROM [LombartDb].[dbo].[Goods]", conn_str);
             DataSet dataSet = new DataSet();
             dataAdapter.Fill(dataSet);
             var reportTable = dataSet.Tables[0];
@@ -52,7 +67,7 @@ namespace Training.Web.Controllers
             for (int j = 0; j < reportTable.Columns.Count; j++)
             {
                 cell = new PdfPCell(new Phrase(new Phrase(reportTable.Columns[j].ColumnName)));
-                cell.BackgroundColor = iTextSharp.text.BaseColor.LIGHT_GRAY;
+                cell.BackgroundColor = BaseColor.LIGHT_GRAY;
                 table.AddCell(cell);
             }
             for (int j = 0; j < reportTable.Rows.Count; j++)
