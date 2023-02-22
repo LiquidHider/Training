1a6108d46bc0c7b75d2bf03a45fa718fccd17ac4
AnBel2804 <anbelov9@gmail.com>
2023-02-22 17:43:17 +0200

    Add Validation

---
+++ Training.Web/Validators/ClientValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Training.Web.Models;
+
+namespace Training.Web.Validators
+{
+    public class ClientValidator : AbstractValidator<Client>
+    {
+        public ClientValidator()
+        {
+            RuleFor(client => client.FullName)
+                .NotEmpty().WithMessage("П.І.Б. клієнта не може бути порожнім")
+                .Length(10, 50).WithMessage("П.І.Б. клієнта має бути від 10 до 50 символів");
+            RuleFor(client => client.PassportSerial)
+                .MaximumLength(8).WithMessage("Серія Паспорту клієнта не може перевищувати 8 символів");
+            RuleFor(client => client.PassportNumber)
+                .NotEmpty().WithMessage("Номер Паспорту клієнта не може бути порожнім")
+                .Length(6, 9).WithMessage("Номер Паспорту клієнта має бути від 6 до 9 символів");
+            RuleFor(client => client.PhoneNumber)
+                .NotEmpty().WithMessage("Номер Телефону клієнта не може бути порожнім")
+                .Matches(@"^\+380\d{9}$").WithMessage("Номер Телефону клієнта невірного формату\n+380*********");
+        }
+    }
+}
---
+++ Training.Web/Validators/GoodValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Training.Web.Models;
+
+namespace Training.Web.Validators
+{
+    public class GoodValidator : AbstractValidator<Good>
+    {
+        public GoodValidator()
+        {
+            RuleFor(good => good.Name)
+                .NotEmpty().WithMessage("Назва товару не може бути порожньою")
+                .Length(3, 50).WithMessage("Назва товару повинна бути від 3 до 50 символів");
+            RuleFor(good => good.Description)
+                .MaximumLength(100).WithMessage("Опис товару не може бути більше 100 символів");
+            RuleFor(good => good.AppraisedValue)
+                .NotEmpty().WithMessage("Оціночна вартість не може бути попрожньою");
+            RuleFor(good => good.Category)
+                .NotNull().WithMessage("Категорія є обов'язковою");
+        }
+    }
+}
